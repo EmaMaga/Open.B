5264f3f0879909425751080728cd1151677333e4
EmaMaga <135460319+EmaMaga@users.noreply.github.com>
2023-07-14 13:11:37 -0300

    Add files via upload

---
+++ IMC.py
@@ -0,0 +1,12 @@
+import math
+peso=float(input('Introduzca su peso '))
+estatura=float(input('Introduzca su estatura '))
+
+a=math.pow(estatura, 2)
+imc=peso/a
+imcRedondeado=round(imc,2)
+print('Su IMC es de: '+str(imcRedondeado))
+
+
+
+
